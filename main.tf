6a28f8ee59117549b82ad846e2b91913b908caa1
Ricardo Lino <52472126+ricardolino83@users.noreply.github.com>
2024-10-23 15:03:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,6 +65,13 @@ resource "aws_security_group" "instance_sg" {
   }
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["18.206.107.24/29"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -98,6 +105,40 @@ output "public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
 
+# Tabelas DynamoDB
+resource "aws_dynamodb_table" "cloudmart_products" {
+  name           = "cloudmart-products"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "cloudmart_orders" {
+  name           = "cloudmart-orders"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "cloudmart_tickets" {
+  name           = "cloudmart-tickets"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
 # AMI Criando a maquina EC2 na AWS
 
 resource "aws_instance" "ec2_instance" {
@@ -114,8 +155,83 @@ resource "aws_instance" "ec2_instance" {
   # Script para instalar o Docker após iniciar a instância
   user_data = <<-EOF
               #!/bin/bash
-              mkdir ricardolino
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ec2-user
 
+              # Criar pasta para descontactar o backend
+              sudo mkdir -p challenge-day2/backend && cd challenge-day2/backend
+
+              # Baixar o arquivo cloudmart-backend.zip
+              sudo wget https://github.com/ricardolino83/terraformdocker/blob/6508e0a8676759b4de9f97ba072201b4f4e881d2/cloudmart-backend.zip
+
+              # Descompactando o backend do sistema
+              sudo unzip cloudmart-backend.zip
+
+              # Criar o arquivo .env do backend
+              cat <<EOT >> .env
+              PORT=5000
+              AWS_REGION=us-east-1
+              BEDROCK_AGENT_ID=<seu-bedrock-agent-id>
+              BEDROCK_AGENT_ALIAS_ID=<seu-bedrock-agent-alias-id>
+              OPENAI_API_KEY=<sua-chave-api-openai>
+              OPENAI_ASSISTANT_ID=<seu-id-assistente-openai>
+              EOT
+
+              # Criar o arquivo Dockerfile
+              cat <<EOT >> Dockerfile
+              FROM node:18
+              WORKDIR /usr/src/app
+              COPY package*.json ./
+              RUN npm install
+              COPY . .
+              EXPOSE 5000
+              CMD ["npm", "start"]
+              EOT
+
+              # Construir e executar a imagem Docker
+              sudo docker build -t cloudmart-backend .
+              sudo docker run -d -p 5000:5000 --env-file .env cloudmart-backend
+
+              cd ..
+
+              # Criar pasta e baixar o código-fonte
+              sudo mkdir frontend && cd frontend
+              sudo wget https://github.com/ricardolino83/terraformdocker/blob/6508e0a8676759b4de9f97ba072201b4f4e881d2/cloudmart-frontend.zip
+              sudo unzip cloudmart-frontend.zip
+
+              # Obter o IP público da instância
+              PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+              # Criar arquivo .env do frontend
+              cat <<EOT >> .env
+              VITE_API_BASE_URL=http://$PUBLIC_IP:5000/api
+              EOT
+
+              #Criando o Dockerfile do frontend
+              cat <<EOT >> Dockerfile
+              FROM node:16-alpine as build
+              WORKDIR /app
+              COPY package*.json ./
+              RUN npm ci
+              COPY . .
+              RUN npm run build
+
+              FROM node:16-alpine
+              WORKDIR /app
+              RUN npm install -g serve
+              COPY --from=build /app/dist /app
+              ENV PORT=5001
+              ENV NODE_ENV=production
+              EXPOSE 5001
+              CMD ["serve", "-s", ".", "-l", "5001"]
+              EOT
+
+              #Construir e executar a imagem Docker:
+              sudo docker build -t cloudmart-frontend .
+              sudo docker run -d -p 5001:5001 cloudmart-frontend
               EOF
 
   tags = {
